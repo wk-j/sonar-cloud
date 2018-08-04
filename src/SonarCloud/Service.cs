a61809b0a38631f68c1d1225f3f99bf597af3aa5
wk-j <somnuk.wk@gmail.com>
2018-08-05 04:48:21 +0700

    Enum

--- src/SonarCloud/Service.cs
+++ src/SonarCloud/Service.cs
@@ -3,6 +3,14 @@ using System.IO;
 
 namespace SonarCloud {
     public class Service {
+
+        enum Color { Green, Blue }
+
+        public static void Enummmm() {
+            var color = Color.Green - 1;
+            Console.WriteLine(color);
+        }
+
         public static void LockNull() {
             lock (null) {
                 Console.WriteLine("This is bug");
